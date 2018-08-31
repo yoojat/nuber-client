1c68afdf4bdfe9334a3b04ff144ee68be1611bce
yoojat <yooja.ty@gmail.com>
2018-08-31 23:13:22 +0900

    Connecting Local State to Components

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ApolloProvider } from "react-apollo";
 import ReactDOM from "react-dom";
 import client from "./apollo";
-import App from "./App";
+import App from "./Components/App"; // App을 따로 만들어 주었음
 
 ReactDOM.render(
   <ApolloProvider client={client}>
