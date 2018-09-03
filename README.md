28e4c0c0dfad7f341576f4ed14426461bc207cd0
yoojat <yooja.ty@gmail.com>
2018-09-04 01:21:28 +0900

    Router and Routes

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # nuber-client
+
 Clone for the(N)uber Clone Course on Nomad Academy. ReactJS, Apollo, Typescript
+
+## Screens:
+
+### Logged Out :
+
+- [ ] Home
+- [ ] Phone Login
+- [ ] Verify Phone Login
+- [ ] Social Login
+
+### Logged In :
+
+- [ ] Home
+- [ ] Ride
+- [ ] Edit Account
+- [ ] Settings
+- [ ] Saved Places
+- [ ] Add Place
+- [ ] Finde Address
+- [ ] Challenge: Ride History
--- src/Components/App/AppPresenter.tsx
+++ src/Components/App/AppPresenter.tsx
@@ -1,13 +1,53 @@
 import PropTypes from "prop-types";
 import React from "react";
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import AddPlace from "../../Routes/AddPlace";
+import EditAccount from "../../Routes/EditAccount";
+import FindAddress from "../../Routes/FindAddress";
+import Home from "../../Routes/Home";
+import OutHome from "../../Routes/OutHome";
+import PhoneLogin from "../../Routes/PhoneLogin";
+import Places from "../../Routes/Places";
+import Ride from "../../Routes/Ride";
+import Settings from "../../Routes/Settings";
+import SocialLogin from "../../Routes/SocialLogin";
+import VerifyPhone from "../../Routes/VerifyPhone";
 
 interface IProps {
   isLoggedIn: boolean;
 }
 
-const AppPresenter: React.SFC<IProps> = ({ isLoggedIn }) =>
-  isLoggedIn ? <span>you are in</span> : <span>you are out</span>;
+const AppPresenter: React.SFC<IProps> = ({ isLoggedIn }) => (
+  <BrowserRouter>
+    {isLoggedIn ? <LoggedInRoute /> : <LoggedOutRoute />}
+  </BrowserRouter>
+);
 
+const LoggedOutRoute: React.SFC = () => (
+  <Switch>
+    <Route path={"/"} exact={true} component={OutHome} />
+    <Route path={"/phone-login"} component={PhoneLogin} />
+    <Route path={"/verify-phone/:number"} component={VerifyPhone} />
+    {/* ex) /verify-phone/01021231222 */}
+    <Route path={"/social-phone"} component={SocialLogin} />
+    <Redirect from={"*"} to={"/"} />
+  </Switch>
+);
+
+const LoggedInRoute: React.SFC = () => (
+  <Switch>
+    <Route path={"/"} exact={true} component={Home} />
+    <Route path={"/ride"} exact={true} component={Ride} />
+    {/* ride also need id but we use method to using memory */}
+    <Route path={"/edit-account"} exact={true} component={EditAccount} />
+    <Route path={"/settings"} exact={true} component={Settings} />
+    <Route path={"/places"} exact={true} component={Places} />
+    <Route path={"/add-place"} exact={true} component={AddPlace} />
+    <Route path={"/find-address"} exact={true} component={FindAddress} />
+    <Redirect from={"*"} to={"/"} />
+    {/* redirect는 맨 마지막에! */}
+  </Switch>
+);
 AppPresenter.propTypes = {
   isLoggedIn: PropTypes.bool.isRequired
 };
