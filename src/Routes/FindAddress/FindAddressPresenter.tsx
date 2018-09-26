8cb0d0baa0084be7518a5e2743e25bc9b4b9023a
yoojat <yooja.ty@gmail.com>
2018-09-26 18:59:42 +0900

    Google Maps and Geolocation

--- src/Routes/FindAddress/FindAddressPresenter.tsx
+++ src/Routes/FindAddress/FindAddressPresenter.tsx
@@ -7,7 +7,22 @@ const Map = styled.div`
   left: 0;
   height: 100%;
   width: 100%;
+  z-index: 1;
 `;
+
+const Center = styled.div`
+  position: absolute;
+  width: 40px;
+  height: 40px;
+  z-index: 2;
+  font-size: 30px;
+  margin: auto;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
 interface IProps {
   mapRef: any;
 }
@@ -19,6 +34,8 @@ class FindAddressPresenter extends React.Component<IProps> {
         <Helmet>
           <title>Find Address | Nuber</title>
         </Helmet>
+        <Center>📍</Center>
+
         <Map innerRef={mapRef} />
       </div>
     );
