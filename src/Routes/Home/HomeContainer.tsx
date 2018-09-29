66b68c0b4bae04a21a5d7995ae4a201e66fa58a1
yoojat <yooja.ty@gmail.com>
2018-09-29 17:14:56 +0900

    HomeScreen Getting Directions

--- src/Routes/Home/HomeContainer.tsx
+++ src/Routes/Home/HomeContainer.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Query } from "react-apollo";
 import ReactDOM from "react-dom";
-import { RouteComponentProps } from "react-router";
+import { RouteComponentProps } from "react-router-dom";
+import { toast } from "react-toastify";
 import { geoCode } from "../../mapHelpers";
 import { USER_PROFILE } from "../../sharedQueries";
 import { userProfile } from "../../types/api";
@@ -14,18 +15,23 @@ interface IState {
   toLng: number;
   lat: number;
   lng: number;
+  distance?: string;
+  duration?: string;
+  price?: number;
 }
-
-class ProfileQuery extends Query<userProfile> {}
 
 interface IProps extends RouteComponentProps<any> {
   google: any;
 }
+
+class ProfileQuery extends Query<userProfile> {}
+
 class HomeContainer extends React.Component<IProps, IState> {
   public mapRef: any;
   public map: google.maps.Map;
   public userMarker: google.maps.Marker;
   public toMarker: google.maps.Marker;
+  public directions: google.maps.DirectionsRenderer;
   public state = {
     isMenuOpen: false,
     lat: 0,
@@ -34,19 +40,16 @@ class HomeContainer extends React.Component<IProps, IState> {
     toLat: 0,
     toLng: 0
   };
-
   constructor(props) {
     super(props);
     this.mapRef = React.createRef();
   }
-
   public componentDidMount() {
     navigator.geolocation.getCurrentPosition(
-      this.handleGeoSuccess,
+      this.handleGeoSucces,
       this.handleGeoError
     );
   }
-
   public render() {
     const { isMenuOpen, toAddress } = this.state;
     return (
@@ -65,20 +68,24 @@ class HomeContainer extends React.Component<IProps, IState> {
       </ProfileQuery>
     );
   }
-
-  public handleGeoSuccess: PositionCallback = (position: Position) => {
+  public toggleMenu = () => {
+    this.setState(state => {
+      return {
+        isMenuOpen: !state.isMenuOpen
+      };
+    });
+  };
+  public handleGeoSucces = (positon: Position) => {
     const {
       coords: { latitude, longitude }
-    } = position;
+    } = positon;
     this.setState({
       lat: latitude,
       lng: longitude
     });
     this.loadMap(latitude, longitude);
   };
-
   public loadMap = (lat, lng) => {
-    console.log(lat, lng);
     const { google } = this.props;
     const maps = google.maps;
     const mapNode = ReactDOM.findDOMNode(this.mapRef.current);
@@ -88,7 +95,6 @@ class HomeContainer extends React.Component<IProps, IState> {
         lng
       },
       disableDefaultUI: true,
-      minZoom: 8,
       zoom: 13
     };
     this.map = new maps.Map(mapNode, mapConfig);
@@ -113,46 +119,36 @@ class HomeContainer extends React.Component<IProps, IState> {
       watchOptions
     );
   };
-  public handleGeoWatchSuccess: PositionCallback = (position: Position) => {
+  public handleGeoWatchSuccess = (position: Position) => {
     const {
       coords: { latitude, longitude }
     } = position;
     this.userMarker.setPosition({ lat: latitude, lng: longitude });
     this.map.panTo({ lat: latitude, lng: longitude });
   };
-
-  public handleGeoWatchError: PositionErrorCallback = () => {
+  public handleGeoWatchError = () => {
     console.log("Error watching you");
   };
-
-  public handleGeoError: PositionErrorCallback = () => {
+  public handleGeoError = () => {
     console.log("No location");
   };
-
   public onInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const {
       target: { name, value }
     } = event;
-
     this.setState({
       [name]: value
     } as any);
   };
-
   public onAddressSubmit = async () => {
     const { toAddress } = this.state;
     const { google } = this.props;
     const maps = google.maps;
     const result = await geoCode(toAddress);
     if (result !== false) {
-      const { lat, lng, formatted_address: formattedAddress } = result;
-      this.setState({
-        toAddress: formattedAddress,
-        toLat: lat,
-        toLng: lng
-      });
+      const { lat, lng, formatted_address: formatedAddress } = result;
       if (this.toMarker) {
-        this.toMarker.setMap(null); // 지도에서 marker 지우기
+        this.toMarker.setMap(null);
       }
       const toMarkerOptions: google.maps.MarkerOptions = {
         position: {
@@ -162,15 +158,59 @@ class HomeContainer extends React.Component<IProps, IState> {
       };
       this.toMarker = new maps.Marker(toMarkerOptions);
       this.toMarker.setMap(this.map);
+      const bounds = new maps.LatLngBounds();
+      bounds.extend({ lat, lng });
+      bounds.extend({ lat: this.state.lat, lng: this.state.lng });
+      this.map.fitBounds(bounds);
+      this.setState(
+        {
+          toAddress: formatedAddress,
+          toLat: lat,
+          toLng: lng
+        },
+        this.createPath
+      );
     }
   };
-
-  public toggleMenu = () => {
-    this.setState(state => {
-      return {
-        isMenuOpen: !state.isMenuOpen
-      };
+  public createPath = () => {
+    const { toLat, toLng, lat, lng } = this.state;
+    if (this.directions) {
+      this.directions.setMap(null);
+    }
+    const renderOptions: google.maps.DirectionsRendererOptions = {
+      polylineOptions: {
+        strokeColor: "#000"
+      },
+      suppressMarkers: true
+    };
+    this.directions = new google.maps.DirectionsRenderer(renderOptions);
+    const directionsService: google.maps.DirectionsService = new google.maps.DirectionsService();
+    const to = new google.maps.LatLng(toLat, toLng);
+    const from = new google.maps.LatLng(lat, lng);
+    const directionsOptions: google.maps.DirectionsRequest = {
+      destination: to,
+      origin: from,
+      travelMode: google.maps.TravelMode.TRANSIT
+    };
+    directionsService.route(directionsOptions, (result, status) => {
+      console.log({ status }, { result });
+      if (status === google.maps.DirectionsStatus.OK) {
+        const { routes } = result;
+        const {
+          distance: { text: distance },
+          duration: { text: duration }
+        } = routes[0].legs[0];
+        this.setState({
+          distance,
+          duration
+        });
+        this.directions.setDirections(result);
+        this.directions.setMap(this.map);
+      } else {
+        toast.error("There is no route there, you have to ");
+      }
     });
   };
 }
+
 export default HomeContainer;
