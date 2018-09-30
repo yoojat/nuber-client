04f295406b93bf26e49f15927cee7c4e3a6f0787
yoojat <yooja.ty@gmail.com>
2018-09-30 17:25:39 +0900

    Ejecting from Apollo Boost

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,56 +1,110 @@
-import ApolloClient, { Operation } from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { ApolloLink, concat, Operation, split } from "apollo-link";
+import { onError } from "apollo-link-error";
+import { HttpLink } from "apollo-link-http";
+import { withClientState } from "apollo-link-state";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
+import { toast } from "react-toastify";
 
 // AppolloClient는 config object를 가져야됨
 // * clientState : defaultState, 언어선택,화폐 등등의 일을 할 수있음
 //   clientState의 resolver는 state(clientState)를 조정할 수 있음
 // * request : 클라이언트가 request할 때마다 생기게 함
 // * uri : 서버의 graphqlAPI를 가지고 있음을 보여줌
-const client = new ApolloClient({
-  clientState: {
-    defaults: {
-      auth: {
-        __typename: "Auth",
-        isLoggedIn: Boolean(localStorage.getItem("jwt")) || false
-      }
-    },
-    resolvers: {
-      Mutation: {
-        // cache는 clientState를 defaults를 뜻함
-        logUserIn: (_, { token }, { cache }) => {
-          localStorage.setItem("jwt", token);
-          cache.writeData({
-            data: {
-              auth: {
-                __typename: "Auth",
-                isLoggedIn: true
-              }
-            }
-          });
-          return null;
-        },
-        logUserOut: (_, __, { cache }) => {
-          localStorage.removeItem("jwt");
-          cache.writeData({
-            data: {
-              auth: {
-                __typename: "Auth",
-                isLoggedIn: false
-              }
-            }
-          });
-          return null;
-        }
-      }
+
+const getToken = () => {
+  const token = localStorage.getItem("jwt");
+  if (token) {
+    return token;
+  } else {
+    return "";
+  }
+};
+const cache = new InMemoryCache();
+const authMiddleware = new ApolloLink((operation: Operation, forward: any) => {
+  operation.setContext({
+    headers: {
+      "X-JWT": getToken()
     }
-  },
-  request: async (operation: Operation) => {
-    operation.setContext({
-      headers: {
-        "X-JWT": localStorage.getItem("jwt") || ""
-      }
-    });
-  },
+  });
+  return forward(operation);
+});
+const httpLink = new HttpLink({
   uri: "http://localhost:4000/graphql"
 });
+const wsLink = new WebSocketLink({
+  options: {
+    connectionParams: {
+      "X-JWT": getToken()
+    },
+    reconnect: true
+  },
+  uri: "ws://localhost:4000/subscriptions"
+});
+const combinedLinks = split(
+  ({ query }) => {
+    const { kind, operation }: any = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message }) => {
+      toast.error(`Unexpected error: ${message}`);
+    });
+  }
+  if (networkError) {
+    toast.error(`Network error: ${networkError}`);
+  }
+});
+const localStateLink = withClientState({
+  cache,
+  defaults: {
+    auth: {
+      __typename: "Auth",
+      isLoggedIn: Boolean(localStorage.getItem("jwt"))
+    }
+  },
+  resolvers: {
+    Mutation: {
+      logUserIn: (_, { token }, { cache: appCache }) => {
+        localStorage.setItem("jwt", token);
+        appCache.writeData({
+          data: {
+            auth: {
+              __typename: "Auth",
+              isLoggedIn: true
+            }
+          }
+        });
+        return null;
+      },
+      logUserOut: (_, __, { cache: appCache }) => {
+        localStorage.removeItem("jwt");
+        appCache.writeData({
+          data: {
+            auth: {
+              __typename: "Auth",
+              isLoggedIn: false
+            }
+          }
+        });
+        return null;
+      }
+    }
+  }
+});
 
+const client = new ApolloClient({
+  cache,
+  link: ApolloLink.from([
+    errorLink,
+    localStateLink,
+    concat(authMiddleware, combinedLinks)
+  ])
+});
 export default client;
